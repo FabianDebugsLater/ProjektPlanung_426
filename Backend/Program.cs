3bcade316726a491043809278f3ae419f775eeb8
Fabiandislikesgibb <162465093+Fabiandislikesgibb@users.noreply.github.com>
2025-06-05 11:22:31 +0200

    mssql connection fix / healthcheck endpoint created

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -13,6 +13,21 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var dbContext = services.GetRequiredService<AppDbContext>();
+        dbContext.Database.EnsureCreated();
+        Console.WriteLine("Database created successfully!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred creating the database: {ex.Message}");
+    }
+}
+
 // Middleware
 app.UseSwagger();
 app.UseSwaggerUI();
