d4fa2a372c6a4abc3ca966a6ae12464daaa82d00
DanielMuerner <daniel.muerner@bluewin.ch>
2025-06-12 08:27:27 +0200

    NuGet package added

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4538d484fa6222e11558250824e9c4eaf14e0293")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+660a0ac2abec09fa352953832633ec9382cb534a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
